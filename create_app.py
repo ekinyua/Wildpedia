76a797f1781acee362c2f38ff750ed75139a867e
Vulgence <e.kinyua@alustudent.com>
2023-11-22 16:09:31 +0300

    migrate

--- create_app.py
+++ create_app.py
@@ -3,16 +3,22 @@ from flask_sqlalchemy import SQLAlchemy
 import os
 from dotenv import load_dotenv
 from flask_login import LoginManager
+from flask_migrate import Migrate
 
 db = SQLAlchemy()
 load_dotenv()
 base_url = os.path.abspath(os.path.dirname(__file__))
 
+uri = os.environ.get('wildpedia_database')
+if uri.startswith('postgres://'):
+    uri = uri.replace('postgres://', 'postgresql://', 1)
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(base_url, 'wildpedia.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = uri
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(app)
+migrate = Migrate(app, db)
 
 from views import views
 from auth import auth
